f64766bef57bfeeda21503760d5a29a2767f0378
Ács Ádám <acsadam0404@gmail.com>
2015-02-17 23:23:15 +0100

    init block duplikált konstruktor helyett

--- kuru-vaadin/src/main/java/hu/kuru/vaadin/component/KWindow.java
+++ kuru-vaadin/src/main/java/hu/kuru/vaadin/component/KWindow.java
@@ -4,19 +4,18 @@ import com.vaadin.ui.Component;
 import com.vaadin.ui.Window;
 
 public class KWindow extends Window {
-
-	public KWindow(String caption, Component comp) {
-		super(caption, comp);
+	{
 		setModal(true);
 		setResizable(false);
 		setDraggable(false);
 	}
+	
+	public KWindow(String caption, Component comp) {
+		super(caption, comp);
+	}
 
 	public KWindow(String caption) {
 		super(caption);
-		setModal(true);
-		setResizable(false);
-		setDraggable(false);
 	}
 
 }
