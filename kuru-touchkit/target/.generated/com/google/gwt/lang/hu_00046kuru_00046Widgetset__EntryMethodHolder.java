15ff62677ac32bd83783324915ac39029ed5e9bc
Ács Ádám <acsadam0404@gmail.com>
2015-03-08 20:06:08 +0100

    törlések

--- kuru-touchkit/target/.generated/com/google/gwt/lang/hu_00046kuru_00046Widgetset__EntryMethodHolder.java
+++
@@ -1,6 +0,0 @@
-package com.google.gwt.lang;
-public class hu_00046kuru_00046Widgetset__EntryMethodHolder {
-  public static final void init() {
-    // Filled in by the compiler to call entry methods.
-  }
-}
