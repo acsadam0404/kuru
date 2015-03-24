e3799214f99d6a98d8c806d7641a51fc36b5bdb5
acsadam0404 <acsadam0404@gmail.com>
2015-03-24 09:53:39 +0100

    converter port (egyelőre ne hivatkozzuk be si-vaadin-t)

---
+++ kuru-vaadin/src/main/java/hu/si/vaadin/converter/StringToLongConverter.java
@@ -0,0 +1,46 @@
+package hu.si.vaadin.converter;
+
+
+import java.util.Locale;
+
+import com.vaadin.server.VaadinSession;
+
+public class StringToLongConverter extends AbstractCustomizableStringToNumberConverter<Long> {
+
+	public StringToLongConverter(String formatString) {
+		super(formatString);
+	}
+
+	@Override
+	public Long convertToModel(String value, Class<? extends Long> targetType, Locale locale) throws com.vaadin.data.util.converter.Converter.ConversionException {
+		if (value != null && value.contains(" ")) {
+			value = value.replace(" ", "");
+		}
+		Number num = super.convertToNumber(value, targetType, locale);
+		if (num != null && !(num instanceof Long)) {
+			num = Long.parseLong(num.toString());
+		}
+
+		return (Long) num;
+	}
+
+	public String convertToPresentation(Long value) throws ConversionException {
+		return super.convertToPresentation(value, String.class, VaadinSession.getCurrent().getLocale());
+	}
+	
+	@Override
+	public Class<Long> getModelType() {
+		return Long.class;
+	}
+
+	private static final StringToLongConverter instance = new StringToLongConverter();
+
+	private StringToLongConverter() {
+		/* singleton */
+	}
+
+	public static StringToLongConverter getInstance() {
+		return instance;
+	}
+
+}
